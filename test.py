aaf81face213ceac0962fa9539e3206d76ffbdef
zyroknight1706 <52271739+zyroknight1706@users.noreply.github.com>
2021-08-07 17:50:12 +0530

    new step, checking in test

---
+++ test.py
@@ -0,0 +1,26 @@
+import sys
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+
+app = QApplication(sys.argv)
+win = QMainWindow()
+win2 = QMainWindow()
+
+win.setGeometry(0, 0, 1920, 1080)
+win.setStyleSheet("background-image: url(C:/Users/Hp/Downloads/newspaper_back.jpg);")
+win.setWindowTitle('Midark!')
+win2.setGeometry(0, 0, 1920, 1080)
+win2.setStyleSheet("background-image: url(C:/Users/Hp/Downloads/newspaper_back_4k.jpg);")
+win2.setWindowTitle('Midark!!')
+app.setActiveWindow(win)
+win.show()
+
+QTimer.start(app, 5000)
+win2.show()
+#app.setStyleSheet("background-image: url(newspaper_back.jpg);")
+'''
+lbl = QLabel('lmaoooooooooooooooooo')
+lbl.setStyleSheet('background-image: url(C:/Users/Hp/Downloads/newspaper_back.jpg);')
+lbl.show()
+'''
+sys.exit(app.exec_())
