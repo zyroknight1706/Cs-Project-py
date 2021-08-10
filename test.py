962fa2e65c765bf57d2ccdc3f071f9fa177cd893
zyroknight1706 <52271739+zyroknight1706@users.noreply.github.com>
2021-08-10 09:37:28 +0530

    new resource file

--- test.py
+++ test.py
@@ -1,25 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
 
 app = QApplication(sys.argv)
 win = QMainWindow()
-win2 = QMainWindow()
-
-win.setGeometry(0, 0, 1920, 1080)
-win.setStyleSheet("background-image: url(C:/Users/Hp/Downloads/sci-fi_back.jpg);")
-win.setWindowTitle('Midark!')
-# win2.setGeometry(0, 0, 1920, 1080)
-# win2.setStyleSheet("background-image: url(C:/Users/Hp/Downloads/newspaper_back_4k.jpg);")
-# win2.setWindowTitle('Midark!!')
-app.setActiveWindow(win)
+win.setWindowTitle("test.... Again")
+uic.loadUi("LoginScreen.ui", win)
 win.show()
-
-#QTimer.start(app, 5000)
-#win2.show()
-#app.setStyleSheet("background-image: url(newspaper_back.jpg);")
-'''
-lbl = QLabel('lmaoooooooooooooooooo')
-lbl.setStyleSheet('background-image: url(C:/Users/Hp/Downloads/newspaper_back.jpg);')
-lbl.show()
-'''
 sys.exit(app.exec_())
